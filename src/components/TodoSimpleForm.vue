8c13eb1c32d8e3186fb25af05b9ecb4b50f89957
jhyounyaho <jhyoun-yaho@hanmail.net>
2022-03-30 07:32:23 +0900

    add TodoList, TodoSimpleForm 추가, 불필요한 소스 제거

---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,60 @@
+<template>
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input 
+            class="form-control mr-2"
+            type="type" 
+            v-model="todo"
+            placeholder="Type new to-do"
+          />
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit">
+            Add
+          </button>
+        </div>
+      </div>
+      <div v-if="hasError" style="color: red">
+        값을 입력해주세요.
+      </div>
+    </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    emits: [
+        'add-todo',
+    ],
+    setup(props, { emit }) {
+        const todo = ref('');
+        const hasError = ref(false);
+
+        const onSubmit = () => {
+            if (todo.value === '') {
+                hasError.value = true;
+            } else {
+                emit('add-todo', {
+                    id: Date.now(),
+                    subject: todo.value,
+                    completed: false,
+                });
+                hasError.value = false;
+                todo.value = '';
+            }
+        }
+
+        return {
+            todo,
+            hasError,
+            onSubmit,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
